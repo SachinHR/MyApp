7034f8b371604b2675e5544fad5b310cd5db0b84
SachinHR <sachinhr.ran@gmail.com>
2020-06-30 13:57:27 +0530

    initial message

---
+++ blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title       = models.CharField(max_length=120)
+    description = models.TextField(blank=True, null=True)
+    price       = models.DecimalField(decimal_places=2, max_digits =100000)
+    summary     = models.TextField(default=" this is cool!",blank=False, null=False)
